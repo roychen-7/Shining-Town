caea5ca6861bba0d7f7f9a45f89ce2e5956ac198
roychad-cy7 <roychad.cy7@gmail.com>
2012-07-03 09:32:04 +0800

    product comment i18n finished

--- protected/controllers/ProductCommentController.php
+++ protected/controllers/ProductCommentController.php
@@ -90,15 +90,15 @@ class ProductCommentController extends Controller
 
 			if(!$productCommentModel->validateProductId($productCommentModel->product_id))
 			{
-				$errorMessage = '该产品不存在，请确认后重新提交';
+				$errorMessage = Yii::t('productComment', 'The product is not exist, please try again!');
 			}
 			if(!$productCommentModel->validateContactMethod())
 			{
-				$errorMessage = '联系方式超出长度，请简略填写联系信息（小于50个字）';
+				$errorMessage = Yii::t('productComment', 'Contact method is too long, please shorten it!');
 			}
 			if(!$productCommentModel->validateSiteMark())
 			{
-				$errorMessage = '评分不在范围之内，请重新输入或与管理员联系';
+				$errorMessage = Yii::t('productComment', 'The mark is out of range, please try again!');
 			}
 			
 			$this->layout = false;
---
+++ protected/messages/zh_cn/productComment.php
@@ -0,0 +1,10 @@
+<?php
+
+return array(
+	//Log
+	'The product is not exist, please try again!'	=> '该产品不存在，请确认后重新提交',
+	'Contact method is too long, please shorten it!'=> '联系方式超出长度，请简略填写联系信息（小于50个字）',
+	'The mark is out of range, please try again!'	=> '评分不在范围之内，请重新输入或与管理员联系',
+);
+
+?>

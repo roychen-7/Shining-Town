1b0a2f0b9f8a5501ea6b0bac0a8811683280aa40
roychad-cy7 <roychad.cy7@gmail.com>
2012-07-03 12:45:29 +0800

    add view order _customer utf-8

---
+++ protected/views/order/_customer.php
@@ -0,0 +1,22 @@
+﻿<?php
+
+$json = array();
+$json['message'] = $message;
+
+if($message===null){
+	$json['order']['id'] = $model->id;
+	$json['order']['order_id'] = $model->order_id;
+	$json['order']['order_state_id'] = $model->order_state_id;
+	$json['order']['create_time'] = $model->create_time;
+	$json['order']['product_id'] = $model->product_id;
+	$json['order']['entered_pid'] = $model->entered_pid;
+	$json['order']['remark'] = $model->remark;
+	$json['order']['order_info'] = $model->order_info;
+	$json['order']['product_name'] = $model->product_name;
+}
+
+$json = JSON($json);
+
+responseJSON($json);
+
+?>

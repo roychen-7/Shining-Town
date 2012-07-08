99c434fd609bcd55cead93031f2adf5673eeeae0
roychad-cy7 <roychad.cy7@gmail.com>
2012-07-08 22:12:27 +0800

    order express id

--- protected/views/order/_customer.php
+++ protected/views/order/_customer.php
@@ -10,7 +10,7 @@ if($message===null){
 	$json['order']['create_time'] = $model->create_time;
 	$json['order']['product_id'] = $model->product_id;
 	$json['order']['entered_pid'] = $model->entered_pid;
-	$json['order']['remark'] = $model->remark;
+	$json['order']['express_id'] = ($json['order']['order_state_id']==='6') ? $model->remark : '';
 	$json['order']['order_info'] = $model->order_info;
 	$json['order']['product_name'] = $model->product_name;
 }
